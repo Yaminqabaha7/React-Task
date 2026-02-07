dad580724867ec296a5c6e7628a1efeab502ba96
yamin <yaminrehan15@gmail.com>
2026-02-07 23:08:08 +0200

    update

--- src/formm.js
+++ src/formm.js
@@ -1,28 +1,101 @@
 import { useState } from "react";
 export default function Form11() {
-  const [forminput, setforminput] = useState({ name: "", email: "" });
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    age: "",
+    genralinfo: "",
+    isstudent: false,
+    city: "",
+    status: "",
+  });
   return (
     <>
       <form
-        onSubmit={(ev) => {
-          ev.preventDefault();
+        className="form"
+        onSubmit={(e) => {
+          e.preventDefault();
+          console.log(form);
         }}
       >
-        <label> name</label>
+        <div className="div1">
+          <div className="field">
+            <label>Name</label>
+            <input
+              type="text"
+              value={form.name}
+              onChange={(e) => setForm({ ...form, name: e.target.value })}
+            />
+          </div>
+
+          <div className="field">
+            <label>Email</label>
+            <input
+              type="email"
+              value={form.email}
+              onChange={(e) => setForm({ ...form, email: e.target.value })}
+            />
+          </div>
+
+          <div className="field">
+            <label>Age</label>
+            <input
+              type="number"
+              value={form.age}
+              onChange={(e) => setForm({ ...form, age: e.target.value })}
+            />
+          </div>
+        </div>
+        <label>is student</label>
         <input
-          value={forminput.name}
-          onChange={(as) => {
-            setforminput({ name: as.target.value });
+          type="checkbox"
+          checked={form.isstudent}
+          onChange={(v) => {
+            setForm({ ...form, isstudent: v.target.checked });
           }}
         />
-        <label> email</label>
-        <input
-          value={forminput.email}
-          onChange={(as) => {
-            setforminput({ email: as.target.value });
+        <select
+          value={form.city}
+          onChange={(v) => {
+            setForm({ ...form, city: v.target.value });
           }}
-        />
-        <button>sumbet</button>
+        >
+          <option>jenin</option>
+          <option>Nablus</option>
+          <option>Tulkarem</option>
+        </select>
+        <div>
+          <input
+            type="radio"
+            value="Sudent"
+            checked={form.status == "Sudent"}
+            onChange={(v) => {
+              setForm({ ...form, status: v.target.value });
+            }}
+          />
+          Sudent
+          <input
+            type="radio"
+            value="Teacher"
+            checked={form.status == "Teacher"}
+            onChange={(v) => {
+              setForm({ ...form, status: v.target.value });
+            }}
+          />
+          Teacher
+        </div>
+        <div className="field">
+          <label>Message</label>
+          <textarea
+            rows="4"
+            value={form.genralinfo}
+            onChange={(e) => {
+              setForm({ ...form, genralinfo: e.target.value });
+            }}
+          />
+        </div>
+
+        <button className="btn">Submit</button>
       </form>
     </>
   );
